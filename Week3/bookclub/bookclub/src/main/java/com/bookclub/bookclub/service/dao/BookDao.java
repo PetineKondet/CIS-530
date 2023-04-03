51f8687fcee17cd4a4c6786b01e24af1ed457664
PetineKondet <112782780+PetineKondet@users.noreply.github.com>
2023-04-02 23:36:28 -0400

    Add files via upload

---
+++ Week3/bookclub/bookclub/src/main/java/com/bookclub/bookclub/service/dao/BookDao.java
@@ -0,0 +1,8 @@
+package com.bookclub.bookclub.service.dao;
+
+import com.bookclub.bookclub.model.Book;
+import com.bookclub.bookclub.service.GenericDao;
+
+public interface BookDao extends GenericDao<Book, String> {
+}
+
